2e37da42462dd62f4ea84480f223a2f5710ac848
z619850002 <619850002@qq.com>
2017-12-22 19:53:13 +0800

    a delete

--- MyBoot/MyBoot/src/main/java/com/example/MyBoot/repository/jparep/ActionRep.java
+++
@@ -1,8 +0,0 @@
-package com.example.MyBoot.repository.jparep;
-
-import com.example.MyBoot.repository.entity.Action;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActionRep extends JpaRepository<Action, String> {
-
-}
--- MyBoot/MyBoot/src/main/java/com/example/MyBoot/repository/jparep/StudentRep.java
+++
@@ -1,8 +0,0 @@
-package com.example.MyBoot.repository.jparep;
-
-import com.example.MyBoot.repository.entity.StudentInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRep extends JpaRepository<StudentInfo ,String > {
-
-}
